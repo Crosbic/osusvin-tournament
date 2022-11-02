e601ffad1c8187419708f6da2dd14c775b52e665
crosbic <thecrosbic@gmail.com>
2022-11-03 02:08:04 +0300

    фикс бг+футер, но ещё надо поработать с этим

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,66 +1,58 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
 
-const Home: NextPage = () => {
+const Home = () => {
   return (
-    <div>
+    <div className={styles.wrapper}>
       <Head>
         <title>osu!svin tournament</title>
       </Head>
-      <div className={styles.background}>
-        <div className={styles.main}>
-          <div className={styles.reggrid}>
-            <a href="" className={styles.regcard}>
-              <h2>Регистрация</h2>
+      <main className={styles.main}>
+        <div className={styles.reggrid}>
+          <a href="" className={styles.regcard}>
+            <h2>Регистрация</h2>
+          </a>
+
+          <div className={styles.grid}>
+            <a href="" className={styles.card}>
+              <h2>Список игроков</h2>
             </a>
 
-            <div className={styles.grid}>
-              <a href="" className={styles.card}>
-                <h2>Список игроков</h2>
-              </a>
+            <a href="" className={styles.card}>
+              <h2>Расписание</h2>
+            </a>
 
-              <a href="" className={styles.card}>
-                <h2>Расписание</h2>
-              </a>
-
-              <a href="" className={styles.card}>
-                <h2>Маппулы</h2>
-              </a>
-            </div>
-            <div className={styles.icons}>
-              <a href="https://discord.gg/KfdDd2zqas">
-                <Image
-                  src="/discord.png"
-                  alt="Discord logo"
-                  width={52}
-                  height={52}
-                />
-              </a>
-              <Image src="/osu.png" alt="Osu logo" width={56} height={52} />
+            <a href="" className={styles.card}>
+              <h2>Маппулы</h2>
+            </a>
+          </div>
+          <div className={styles.icons}>
+            <a href="https://discord.gg/KfdDd2zqas">
               <Image
-                src="/twitch.png"
-                alt="Twitch logo"
+                src="/discord.png"
+                alt="Discord logo"
                 width={52}
                 height={52}
               />
-            </div>
+            </a>
+            <Image src="/osu.png" alt="Osu logo" width={56} height={52} />
+            <Image src="/twitch.png" alt="Twitch logo" width={52} height={52} />
           </div>
         </div>
+      </main>
 
-        <div className={styles.footer}>
-          <a
-            href="https://vk.com/cheeky_breeky_i_v_damke"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            По всем вопросам обращаться сюда
-          </a>
-        </div>
-      </div>
+      <footer>
+        <a
+          href="https://vk.com/cheeky_breeky_i_v_damke"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          По всем вопросам обращаться сюда
+        </a>
+      </footer>
     </div>
-  );
-};
+  )
+}
 
-export default Home;
+export default Home
