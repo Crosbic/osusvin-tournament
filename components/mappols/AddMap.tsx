70efd96450f6a86a21850794a97777392401c3dd
Uladzislau Sobal <sobolvladsav@gmail.com>
2023-05-05 11:22:27 +0300

    fix value params in callback; Function type removed

--- components/mappols/AddMap.tsx
+++ components/mappols/AddMap.tsx
@@ -8,35 +8,39 @@ import {
 import React, { useState } from 'react'
 
 interface AddMapProps {
-  callBack: Function
+  callBack: any
   index: number
 }
 
 const AddMap = (props: AddMapProps) => {
-  const {callBack, index} = props;
+  const { callBack, index } = props;
   const [beatmapUrl, setBeatmapUrl] = useState<string>('')
   const [tournamentMod, setTournamentMod] = useState<string>('')
   const [tournamentModName, setTournamentModName] = useState<string>('')
   const mods = ['NM', 'HD', 'HR', 'DT', 'FM', 'TB']
 
   const onChangeHandler = (e: any, valueName: string) => {
-    const value = e.target.value   
+    const value = e.target.value
+    const newData = {
+      beatmapUrl: beatmapUrl,
+      tournamentMod: tournamentMod,
+      tournamentModName: tournamentModName
+    };
     switch (valueName) {
       case "BeatmapUrl":
         setBeatmapUrl(value)
-        callBack({value, tournamentMod, tournamentModName}, index)
-      break;
+        newData.beatmapUrl = value
+        break;
       case "TournamentMod":
         setTournamentMod(value)
-        callBack({beatmapUrl, value, tournamentModName}, index)
-      break;
+        newData.tournamentMod = value
+        break;
       case "TournamentModName":
         setTournamentModName(value)
-        callBack({beatmapUrl, tournamentMod, value}, index)
-      break;  
+        newData.tournamentModName = value
+        break;
     }
-    // !!! not working; changed states not handled
-    //callBack({beatmapUrl, tournamentMod, tournamentModName}, index)    
+    callBack(newData, index)
   }
 
   return (
@@ -51,7 +55,7 @@ const AddMap = (props: AddMapProps) => {
         <InputLabel variant="outlined">Статы по моду</InputLabel>
         <Select
           label="Статы по моду"
-          onChange={(e) =>  onChangeHandler(e, "TournamentMod")}
+          onChange={(e) => onChangeHandler(e, "TournamentMod")}
           value={tournamentMod}
           required
         >
@@ -69,7 +73,7 @@ const AddMap = (props: AddMapProps) => {
         value={tournamentModName}
         onChange={(e) => onChangeHandler(e, "TournamentModName")}
         required
-      /> 
+      />
     </>
   )
 }
